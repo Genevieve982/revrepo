63237d2e8bb85eb133151f98a50fef669b915864
Genevieve Scholler <scholler.genevieve@gmail.com>
2022-05-18 23:02:38 -0500

    Updated all files

---
+++ Calc.java
@@ -0,0 +1,11 @@
+public class Calc {
+    public int add(int 1, int 2){
+        return(n1 + n2);
+    }
+    
+    public int sub(int 1, int 2){
+        return(n1 - n2);
+    }
+
+
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+this is read me file
+
+added calc with add & sub
